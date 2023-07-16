f30b5135267edf75b655b9dade61ac45d6d071c5
ZzzzzzzSkyward <zhu.zheng.sheng@163.com>
2023-07-16 14:57:50 +0800

    add expand.py

--- pyscripts/crop.py
+++ pyscripts/crop.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import sys
-
+from expand import expand
 
 def crop(filename, x, y, nosave=False):
     # open image
@@ -32,11 +32,40 @@ def crop(filename, x, y, nosave=False):
         img_cropped.save(filename)
     return x_final,y_final
 
+def uncrop(filename, x, y, nosave=False):
+    # 打开图片
+    img = Image.open(filename)
+    w, h = img.size
+    
+    # 计算出映射后图片的最小宽高     
+    new_width = int(2 * max(x,1-x) * w)
+    new_height = int(2 * max(y,1-y) * h)
+    #放大画布以容纳原图片
+    min_width=new_width
+    min_height=new_height
+    if min_height==h and min_width==w:
+        return
+    
+    # 新建 RGBA 格式图片    
+    new_img = Image.new('RGBA', (min_width, min_height))
+    
+    # 计算偏移量,使(x, y)变为中心      
+    offset_x = 0 if x>0.5 else min_width-w
+    offset_y = 0 if y>0.5 else min_height-h
+            
+    # 粘贴图片      
+    new_img.paste(img,(offset_x, offset_y))
+  
+    # Save new image  
+    if not nosave:
+        new_img.save(filename)
+
+    return 0.5,0.5
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
         print(
-            "Usage: python crop.py [filename] [pivot_x] [pivot_y] [nosave]\n Crop the image and calculate the new pivot point coordinates.\nnosave: do not crop image."
+            "Usage: python crop.py [filename] [pivot_x] [pivot_y] [nosave] [uncrop]\n Crop the image and calculate the new pivot point coordinates.\nnosave: do not crop image.\nuncrop: crop image so that pivot is (0.5,0.5)"
         )
         exit(1)
     im = sys.argv[1]
@@ -45,4 +74,8 @@ if __name__ == "__main__":
     py = float(sys.argv[3])
     y = 1 - py
     nosave = len(sys.argv) > 4 and sys.argv[4][0] == "n"
-    crop(im, x, y, nosave)
+    un = len(sys.argv) > 5 and sys.argv[5][0] == "u"
+    if un:
+        uncrop(im,x,y,nosave)
+    else:
+        crop(im, x, y, nosave)
---
+++ pyscripts/expand.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import sys
+
+def expand(filename, x, y,ratio=2):
+    # open image
+    img = Image.open(filename)
+    width, height = img.size
+    new_width = width * ratio
+    new_height = height * ratio
+    image_expand = Image.new("RGBA", (new_width, new_height))
+    image_expand.paste(img, (int(width * x), int(height * y)))
+    image_expand.save(filename)
+    return x,y
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print(
+            "Usage: python expand.py [filename] [pivot_x] [pivot_y] [ratio]\n expand the image by ratio, default is 2"
+        )
+        exit(1)
+    im = sys.argv[1]
+    px = float(sys.argv[2])
+    x = px
+    py = float(sys.argv[3])
+    y = 1 - py
+    r=float(sys.argv[4] if len(sys.argv)>4 else 2)
+    expand(im, x, y, r)
